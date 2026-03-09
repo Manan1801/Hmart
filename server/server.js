34125418938dc57e6c2ff7de1245871495664d63
Manan1801 <mananchavda18@gmail.com>
2026-03-09 23:30:15 +0530

    Database

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import pool from './configs/postgresSQL.js';
 
 // Application Configuration    
 const PORT = process.env.PORT || 1504;
 const app = express();
+await pool.connect().then(() => console.log('Connected to PostgreSQL database'))
+.catch(err => console.error('Error connecting to PostgreSQL database:', err));
 
 // Middleware
 app.use(express.json());
